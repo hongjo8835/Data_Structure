c427bce995776f319e54a474ef8b0881ec2651cf
KimHyeongMin <50308836@hanmail.net>
2020-08-13 01:43:56 +0900

    Initial Commit

---
+++ DataStructure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by KHM on 2020/08/13.
+//  Copyright © 2020 KHM. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
